06e9cdac6280d8d90fc70ba06b9a2e613fade9e6
Albert <ramirezalbert3@gmail.com>
2018-10-24 17:05:18 +0200

    tested taxi-v2, also learnable
    cleaned up a bit main

--- main.py
+++ main.py
@@ -2,44 +2,27 @@ import gym
 import numpy as np
 from gym import logger
 from dqn_agent import DQNAgent
-from table_agent import TableAgent
-from runner import linear_decay_epsilon, quadratic_decay_epsilon, constant_decay_epsilon, run_episode, run_epoch
-
-from gym.envs.registration import register
-register(
-    id='FrozenLakeNotSlippery-v0',
-    entry_point='gym.envs.toy_text:FrozenLakeEnv',
-    kwargs={'map_name' : '4x4', 'is_slippery': False},
-    max_episode_steps=100,
-    reward_threshold=0.78, # optimum = .8196
-)
+from runner import constant_decay_epsilon, run_epoch
 
 logger.set_level(logger.INFO)
-# env = gym.make('Taxi-v2')
-env = gym.make('FrozenLake-v0')
+env = gym.make('Taxi-v2')
+# env = gym.make('FrozenLake-v0')
 env.seed(0)
 
-agent = DQNAgent(env.action_space.n, env.observation_space.n, gamma=0.9)
+agent = DQNAgent(env.action_space.n, env.observation_space.n, gamma=0.95)
 
-epochs = 60
-episodes = 400
+epochs = 30
+episodes = 600
 
 # train
 for e in range(epochs):
-    epsilon = constant_decay_epsilon(e, initial_epsilon=1, decay_rate=0.9, min_epsilon=0.01)
-    run_epoch(env, agent, epsilon, e, episodes)
+    epsilon = constant_decay_epsilon(e, initial_epsilon=1, decay_rate=0.8, min_epsilon=0.01)
+    run_epoch(env, agent, epsilon, e, episodes, max_episode_steps=200)
 
 # demonstrate
-rewards = []
-demonstration = 100
-for i in range(demonstration):
-    r, _, _ = run_episode(env, agent, epsilon=0, max_iterations=100, training=False)
-    rewards.append(r)
-logger.info('Demonstration over {} episodes with average reward/episode = {:.3}'.format(demonstration,
-                                                                                        np.mean(rewards)))
-# debug
-agent.print_q_map()
+run_epoch(env, agent, epsilon=0, epoch=None, episodes=100, max_episode_steps=100, training=False)
 
-run_episode(env, agent, 0, training=False, render=True)
+# debug
+# agent.print_q_map()
 
 agent.save()
--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 from dqn_agent import DQNAgent
-from runner import run_episode
+from runner import run_episode, run_epoch
 import gym
+from gym import logger
+
+logger.set_level(logger.INFO)
+
 agent = DQNAgent.from_h5()
-env = gym.make('FrozenLake-v0')
-run_episode(env, agent, 0, training=False, render=True)
+env = gym.make('Taxi-v2')
+
+# Render
+# run_episode(env, agent, 0, training=False, render=True)
+
+# Demonstrate
+run_epoch(env, agent, epsilon=0, epoch=None, episodes=100, max_episode_steps=100, training=False)
