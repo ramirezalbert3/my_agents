72c8924a030876be79d51d7dbe879d7850a00803
Albert <ramirezalbert3@gmail.com>
2018-10-27 18:05:10 +0200

    actually working visualization example

--- core/visualization.py
+++ core/visualization.py
@@ -6,7 +6,7 @@ def rolling_mean(history: list, window: int = 20):
     compiled_rewards = []
     for time, epsilon, num_episodes, rewards, steps, aborted_episodes in history:
         compiled_rewards += (rewards)
-    s = pd.Dataframe(compiled_rewards)
+    s = pd.Series(compiled_rewards)
     rolling_mean = s.rolling(window=window).mean()
     ax = sns.lineplot(data=rolling_mean)
     plt.show()
