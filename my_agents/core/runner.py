4909d5b5021809a840fe52ca56f20cfd502ed107
ramirezalbert3 <ramirezalbert@gmail.com>
2018-11-30 11:52:42 +0100

    changes to episode info-reporting
    some pep8 to runner

--- my_agents/core/runner.py
+++ my_agents/core/runner.py
@@ -16,6 +16,7 @@ This is better because:
 - agents do not to keep track of trained steps, as it is fed to train()
 """
 
+
 def constant_decay_epsilon(epoch: int,
                            initial_epsilon: float = 1,
                            decay_rate: float = 0.9,
@@ -23,10 +24,11 @@ def constant_decay_epsilon(epoch: int,
     epsilon = initial_epsilon * decay_rate ** epoch
     return max(epsilon, min_epsilon)
 
+
 class Runner:
-    
+
     def __init__(self, env, serializer, agent,
-                 epsilon_policy = lambda e: constant_decay_epsilon(e),
+                 epsilon_policy=lambda e: constant_decay_epsilon(e),
                  training_period: int = 50,
                  max_episode_steps: int = 200):
         self._env = env
@@ -38,47 +40,49 @@ class Runner:
         self._epochs_trained = 0
         self._train_period = training_period
         self._train_steps = 0
-    
+
     def warm_up(self, num_steps: int = 128):
         """ "warm-up" agents by initially populating memories with random actions """
         state = self._env.reset()
         for s in range(num_steps):
-                action = self._env.action_space.sample()
-                previous_state = state
-                state, reward, done, _ = self._env.step(action)
-                self._agent.process_observation(self._serializer.serialize(previous_state), action,
-                                                reward, self._serializer.serialize(state), done)
-                if done:
-                    state = self._env.reset()
-    
+            action = self._env.action_space.sample()
+            previous_state = state
+            state, reward, done, _ = self._env.step(action)
+            self._agent.process_observation(self._serializer.serialize(previous_state), action,
+                                            reward, self._serializer.serialize(state), done)
+            if done:
+                state = self._env.reset()
+
     def train(self, num_epochs: int, num_episodes: int, render_frequency: int = 0):
         for _ in range(num_epochs):
             epsilon = self._epsilon_policy(self._epochs_trained)
             t, rewards, steps, aborted_episodes = self.run_epoch(epsilon, num_episodes,
                                                                  training=True, render_frequency=render_frequency)
             logger.info(
-                '({:5.2f}s) ==> Epoch {:2d}: epsilon = {:4.2f} Average reward/episode = {:5.2f} '
-                'Average steps/episode = {:5.1f} with {} aborted episodes'.format(
-                    t, self._epochs_trained, epsilon, np.mean(rewards), np.mean(steps), aborted_episodes))
+                '{:5.2f}s - {:5.1f}ms/ep ==> Epoch {:2d}: epsilon = {:4.2f} | Average reward/episode = {:5.2f} | '
+                'Average steps/episode = {:5.1f} | {:4.1f}%% ({}) aborted episodes'.format(
+                    t, t * 1000 / num_episodes, self._epochs_trained, epsilon, np.mean(rewards), np.mean(steps),
+                    aborted_episodes * 100 / num_episodes, aborted_episodes))
             self._epochs_trained += 1
         return self.history
 
     def demonstrate(self, num_episodes: int):
         t, rewards, steps, aborted_episodes = self.run_epoch(epsilon=0, num_episodes=num_episodes, training=False)
         logger.info(
-            '({:5.2f}s) ==> Demonstration over {:3d} episodes: Average reward/episode = {:5.2f} '
-            'Average steps/episode = {:5.1f} with {} aborted episodes'.format(
-                t, num_episodes, np.mean(rewards), np.mean(steps), aborted_episodes))
+            '{:5.2f}s - {:5.1f}ms/ep ==> Demonstration over {:3d} episodes: Average reward/episode = {:5.2f} | '
+            'Average steps/episode = {:5.1f} | {:4.1f}%% ({}) aborted episodes'.format(
+                t, t * 1000 / num_episodes, num_episodes, np.mean(rewards), np.mean(steps),
+                aborted_episodes * 100 / num_episodes, aborted_episodes))
         return t, np.mean(rewards), np.mean(steps), aborted_episodes
-    
+
     def render(self):
         total_reward, done, steps = self.run_episode(epsilon=0, training=False, render=True)
         return total_reward, done, steps
-    
+
     @property
     def history(self):
         return self._history
-    
+
     def run_episode(self, epsilon: float, training: bool = True, render: bool = False):
         state = self._env.reset()
         if render:
@@ -105,12 +109,12 @@ class Runner:
         if training and h is not None:
             self._history = self._history.append({'epsilon': epsilon,
                                                   'reward': total_reward,
-                                                  'steps': step+1,
+                                                  'steps': step + 1,
                                                   'aborted': not done,
                                                   'loss': np.mean(h.history['loss'])},  # mean across num_epochs
                                                  ignore_index=True)
-        return total_reward, done, step+1
-    
+        return total_reward, done, step + 1
+
     def run_epoch(self, epsilon: float, num_episodes: int, training: bool = True, render_frequency: int = 0):
         rewards = []
         steps = []
@@ -120,15 +124,15 @@ class Runner:
             logger.warn('render_frequency: {} >= num_episodes: {}'.format(render_frequency, num_episodes))
         for i in range(num_episodes):
             render = False
-            if render_frequency != 0 and (i+1) % render_frequency == 0:
+            if render_frequency != 0 and (i + 1) % render_frequency == 0:
                 render = True
             r, done, step = self.run_episode(epsilon, training, render)
-            if render_frequency != 0 and (i+1) % render_frequency == 0:
+            if render_frequency != 0 and (i + 1) % render_frequency == 0:
                 logger.info('Rendered example episode ({} steps) with reward: {:6.2f}'.format(step, r))
             rewards.append(r)
             steps.append(step)
             if not done:
                 aborted_episodes += 1
         end = time.time()
-        
-        return end-start, rewards, steps, aborted_episodes
+
+        return end - start, rewards, steps, aborted_episodes
