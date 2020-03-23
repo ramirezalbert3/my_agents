fd8ffc8c2f157292b4d309ab1899326007aea726
Albert Ramirez <albert.ramirez@bewica.com>
2020-03-23 20:07:01 +0100

    basic updates

--- my_agents/main.py
+++ my_agents/main.py
@@ -1,28 +1,28 @@
 import gym
 from gym import logger
-import agents
+from agents import ddqn_agent
 from core.states import StateSerializer
 from core.runner import constant_decay_epsilon, Runner
 from core.visualization import rolling_mean
 
-'''
-try:
-    import yappi
-except:
-    pass
-'''
+# try:
+#     import yappi
+# except:
+#     pass
 
 logger.set_level(logger.INFO)
 
-env_name = 'CartPole-v0'  # 'CartPole-v0'  'Taxi-v2'    'FrozenLake-v0'
+# env_name = 'CartPole-v0'
+# env_name = 'Taxi-v2'
+env_name = 'FrozenLake-v0'
 env = gym.make(env_name)
 env.seed(0)
 
-serializer = StateSerializer(env.observation_space.shape)
-# serializer = StateSerializer.from_num_states(env.observation_space.n)
+# serializer = StateSerializer(env.observation_space.shape)
+serializer = StateSerializer.from_num_states(env.observation_space.n)
 
 # agent = agents.DQNAgent(env.action_space.n, serializer.shape, gamma=0.85)
-agent = agents.DDQNAgent(env.action_space.n, serializer.shape, gamma=0.95)
+agent = ddqn_agent.DDQNAgent(env.action_space.n, serializer.shape, gamma=0.95)
 # agent = agents.PrioritizedDDQNAgent(env.action_space.n, serializer.shape, gamma=0.95)
 # agent = agents.NStepDDQNAgent(env.action_space.n, serializer.shape, update_horizon=3, gamma=0.95)
 # agent = agents.DistributionalAgent(env.action_space.n, serializer.shape, v_min=0, v_max=100, num_atoms=51, gamma=0.95)
@@ -31,12 +31,12 @@ agent = agents.DDQNAgent(env.action_space.n, serializer.shape, gamma=0.95)
 epochs = 10
 episodes = 200
 
-try:
-    yappi.set_clock_type('cpu')
-    yappi.start(builtins=True)
-    logger.info('Profiling training')
-except:
-    pass
+# try:
+#     yappi.set_clock_type('cpu')
+#     yappi.start(builtins=True)
+#     logger.info('Profiling training')
+# except:
+#     pass
 
 runner = Runner(env, serializer, agent,
                 epsilon_policy=lambda e: constant_decay_epsilon(e,
@@ -49,11 +49,11 @@ runner = Runner(env, serializer, agent,
 runner.warm_up()
 history = runner.train(epochs, episodes)
 
-try:
-    stats = yappi.get_func_stats()
-    stats.save('callgrind.out', type='callgrind')
-except:
-    pass
+# try:
+#     stats = yappi.get_func_stats()
+#     stats.save('callgrind.out', type='callgrind')
+# except:
+#     pass
 
 # demonstrate
 results = runner.demonstrate(num_episodes=100)
